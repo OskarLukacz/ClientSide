413185a181b46cea95e0ae803fc39d1399df2160
Oskar Lukacz <olukacz8531@jh-mba158-23.d214.org>
2017-11-28 11:35:23 -0600

    Initial Commit

---
+++ ClientSide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClientSide
+//
+//  Created by Oskar Lukacz on 11/28/17.
+//  Copyright © 2017 Oskar Lukacz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
